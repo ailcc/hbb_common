9c142838f24a2f58b4e1a64116a732b886921871
jackliu <admin@ailcc.com>
2025-08-06 12:09:23 +0800

    update KEY and Server

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["desk.ailcc.com"];
+pub const RS_PUB_KEY: &str = "jk3wyGjGrn7GLnhXSt6lpgDItpd9lKIuN6CXacxUQsw=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
